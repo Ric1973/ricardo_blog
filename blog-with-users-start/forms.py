c370b2270bf02bd546946e4c9592ba24e3a1dcfe
Ric1973 <81658536+Ric1973@users.noreply.github.com>
2021-06-24 13:36:45 -0500

    Initial Commit

---
+++ blog-with-users-start/forms.py
@@ -0,0 +1,31 @@
+from flask_ckeditor import CKEditorField
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, URL
+
+
+##WTForm
+class CreatePostForm(FlaskForm):
+    title = StringField("Blog Post Title", validators=[DataRequired()])
+    subtitle = StringField("Subtitle", validators=[DataRequired()])
+    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+    body = CKEditorField("Blog Content", validators=[DataRequired()])
+    submit = SubmitField("Submit Post")
+
+
+class RegisterForm(FlaskForm):
+    email = StringField(label="Email", validators=[DataRequired()], render_kw={"placeholder": "Email"})
+    password = PasswordField(label="Password", validators=[DataRequired()], render_kw={"placeholder": "Password"})
+    name = StringField(label="Name", validators=[DataRequired()], render_kw={"placeholder": "Name"})
+    submit = SubmitField(label="Sign me up!")
+
+
+class LoginForm(FlaskForm):
+    email = StringField(label="Email", validators=[DataRequired()], render_kw={"placeholder": "Email"})
+    password = PasswordField(label="Password", validators=[DataRequired()], render_kw={"placeholder": "Password"})
+    submit = SubmitField(label="Let me in!")
+
+
+class CommentForm(FlaskForm):
+    body = CKEditorField("Blog Content", validators=[DataRequired()])
+    submit = SubmitField("Submit Comment")
